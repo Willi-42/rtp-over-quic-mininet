42391f0302d945e407bcb2f6e853e475b9db519e
Fabian Fromwald <fabian@fromwald.at>
2024-01-17 08:15:52 +0100

    Update plot.sh to test repetition

--- plot.sh
+++ plot.sh
@@ -2,13 +2,16 @@ set -x
 
 for i in {0..8} ;
 do 
-	if [ -d "data/$i/" ]
-	then
-		./plot.py --capacity data/$i/capacity.log --rtp-received data/$i/receiver_rtp.log --rtp-sent data/$i/sender_rtp.log --cc data/$i/cc.log --rtcp-received data/$i/sender_rtcp.log --rtcp-sent data/$i/receiver_rtcp.log --config data/$i/config.json -o $i\_rates.png &
-		./plot.py --qdelay data/$i/cc.log -o $i\_qdelay.png &
-		./plot.py --loss data/$i/sender_rtp.log data/$i/receiver_rtp.log --config data/$i/config.json -o $i\_loss.png &
-		./plot.py --latency data/$i/sender_rtp.log data/$i/receiver_rtp.log --config data/$i/config.json -o $i\_latency.png &
-	fi
+	for j in {1..20} ;
+	do
+		if [ -d "data/$i-$j/" ]
+		then
+			./plot.py --capacity data/$i-$j/capacity.log --rtp-received data/$i-$j/receiver_rtp.log --rtp-sent data/$i-$j/sender_rtp.log --cc data/$i-$j/cc.log --rtcp-received data/$i-$j/sender_rtcp.log --rtcp-sent data/$i-$j/receiver_rtcp.log --config data/$i-$j/config.json -o $i-$j\_rates.png &
+			./plot.py --qdelay data/$i-$j/cc.log -o $i-$j\_qdelay.png &
+			./plot.py --loss data/$i-$j/sender_rtp.log data/$i-$j/receiver_rtp.log --config data/$i-$j/config.json -o $i-$j\_loss.png &
+			./plot.py --latency data/$i-$j/sender_rtp.log data/$i-$j/receiver_rtp.log --config data/$i-$j/config.json -o $i-$j\_latency.png &
+		fi
+	done
 done
 
 wait
